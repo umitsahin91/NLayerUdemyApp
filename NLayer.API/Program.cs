ee25182a0d26291aed22fd8ee0e2922bc4492957
Umit Sahin <umit.sahin91@hotmail.com>
2022-06-01 14:28:21 +0300

    Service layer and dto's

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-//builder.Services.AddScoped(typeof(IService<>), typeof(Ser<>));
+//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
--- Nlayer.Core/Services/IService.cs
+++ Nlayer.Core/Services/IService.cs
@@ -15,8 +15,8 @@ namespace Nlayer.Core.Services
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
 
 
-        Task AddAsync(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
         Task RemoveRangeAsync(IEnumerable<T> entities);

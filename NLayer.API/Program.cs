7073dc4b543488dc644896c99f7dd39eb04c4a44
Umit Sahin <umit.sahin91@hotmail.com>
2022-06-01 23:05:31 +0300

    automapper and customaresponse

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -5,6 +5,8 @@ using Nlayer.Core.UnitOfWorks;
 using NLayer.Repository;
 using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitofWork;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +20,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Nlayer.Core;
+using Nlayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product,ProductDto>().ReverseMap();
+            CreateMap<Category,CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+
+        }
+    }
+}

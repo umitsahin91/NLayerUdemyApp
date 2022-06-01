f76ff6e29bb5c11ce9215d4f37d55e2c3f87cbd4
Umit Sahin <umit.sahin91@hotmail.com>
2022-06-01 11:20:54 +0300

    Repository layer

--- Nlayer.Core/BaseEntity.cs
+++ Nlayer.Core/BaseEntity.cs
@@ -10,5 +10,5 @@ public abstract class BaseEntity
 {
     public int Id { get; set; }
     public DateTime CreatedDate { get; set; }
-    public DateTime UpdatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
 }
